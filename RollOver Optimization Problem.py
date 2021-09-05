b081ec6fe35ef70de843fd8379683fe75b6cecae
Cataluna <catalunalilith2680@gmail.com>
2021-09-05 13:34:27 -0400

    innitial commit of old code associated with MIT OCW's 6.0002 coursework

--- RollOver Optimization Problem.py
+++ RollOver Optimization Problem.py
@@ -9,6 +9,7 @@ Constraints:
 a	+	b	+	c	+	d	+	e	≥	20
 """
 
+
 def genSubsets(L):
     """L is a list of elements
     returns the powerset of L"""
@@ -19,38 +20,39 @@ def genSubsets(L):
     new = []
     for small in smaller:
         new.append(small + extra)
-    return smaller + new 
+    return smaller + new
 
 
 def RollOver(F, ps1, ps2, ps3, ps4, ps5):
-    """returns list [a, b, c, d, e] representing the values to give a maximal score given the inputed grades
+    """returns list [a, b, c, d, e]
+    representing the values to give a maximal score given the inputed grades
     Score=((60–(a+b+c+d+e))*F + a*ps1 + b*ps2 + c*ps3 + d*ps4 + e*ps5
     a,b,c,d,e must be 10 or 0
     a+b+c+d+e >= 20"""
-    best_score = 0 
+    best_score = 0
     power_set = genSubsets(["a", "b", "c", "d", "e"])
     power_set_copy = power_set.copy()
-    #remove elements that dont meet criteria a+b+c+d+e >= 20
+    # remove elements that dont meet criteria a+b+c+d+e >= 20
     for elem in power_set_copy:
-         if len(elem)<2:
-             power_set.remove(elem)
-    #iterates over remainign power_set to find set that gives highest score
+        if len(elem) < 2:
+            power_set.remove(elem)
+    # iterates over remainign power_set to find set that gives highest score
     for subset in power_set:
-        #create list of values of ["a", "b", "c", "d", "e"]
+        # create list of values of ["a", "b", "c", "d", "e"]
         subset_values = []
         for char in "abcde":
             if char in subset:
                 subset_values.append((10))
-            else: 
+            else:
                 subset_values.append((0))
-        #calculate score 
+        # calculate score
         a = subset_values[0]
         b = subset_values[1]
         c = subset_values[2]
         d = subset_values[3]
         e = subset_values[4]
         subset_score = (60-(a+b+c+d+e))*F + a*ps1 + b*ps2 + c*ps3 + d*ps4 + e*ps5
-        #update bests
+        # update bests
         if subset_score > best_score:
             best_score = subset_score
             best_set = subset
